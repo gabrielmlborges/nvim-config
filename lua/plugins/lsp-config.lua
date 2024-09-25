63bf1baf7dd597d2abf8b18adc491cffb8a50e2c
Gabriel Borges <159583298+gabrielmlborges@users.noreply.github.com>
2024-09-25 13:57:14 -0300

    add Mason

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,9 +1,43 @@
 return {
-    "neovim/nvim-lspconfig",
-    config = function()
-        local capabilities = require('cmp_nvim_lsp').default_capabilities()
-        require("lspconfig").lua_ls.setup {
-            capabilities = capabilities
-        }
-    end
+    {
+        "williamboman/mason.nvim",
+        opts = {},
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function ()
+            require("mason-lspconfig").setup({
+                ensure_installed = {"lua_ls"}
+            })
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            require("mason-lspconfig").setup_handlers {
+                function (server_name) -- default handler (optional)
+                    require("lspconfig")[server_name].setup {
+                        capabilities = capabilities
+                    }
+                end,
+                ["lua_ls"] = function ()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.lua_ls.setup {
+                        settings = {
+                            Lua = {
+                                diagnostics = {
+                                    globals = { "vim" }
+                                }
+                            }
+                        }
+                    }
+                end,
+            }
+        end
+    },
+    {
+        "neovim/nvim-lspconfig"
+        --[[config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            require("lspconfig").lua_ls.setup {
+                capabilities = capabilities
+            }
+        end]]
+    }
 }
